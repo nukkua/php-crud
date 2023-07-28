557125b606d76a74437132401aee864498400cd9
Leverna <marco.salazar.2014.com@gmail.com>
2023-07-28 02:02:59 -0400

    a

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mysql Crud</title>
+    <!-- BOOTSRAP 4 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <!-- FONT AWESOME 5 -->
+    <script src="https://kit.fontawesome.com/02f49fd3e0.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+
+        </div>
+
+    </nav>
